6b12c45311f2cc665bf9253ac93f3e4868b881ec
dahyun <gimd15629@gmail.com>
2025-03-18 22:31:47 +0900

    fix: keyword cache 비트마스크 방식으로 변경

--- src/utils/cache_response.py
+++ src/utils/cache_response.py
@@ -2,7 +2,6 @@
 import sqlite3
 from utils.recommendation import makeRecommendPrompt
 from utils.vector_db import search_vec
-from langchain_openai import ChatOpenAI
 import os
 from typing import List
 from dotenv import load_dotenv
@@ -38,7 +37,7 @@ def listArray(keywords, n, result, start=0, current=[]):
 # 키워드로 캐싱된 응답 불러오는 함수
 def get_cached_response(ctg1, ctg2):
     ctg2_bit = generate_bitmask(ctg1, ctg2)
-    
+
     conn = sqlite3.connect(cache_path)
     cursor = conn.cursor()
     sql = "SELECT response FROM keyword_cache WHERE category=? AND bitmask=?"
